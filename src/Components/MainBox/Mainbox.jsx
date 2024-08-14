d941f41b24a92a656b636c5812033ac88ebdecbd
rohit chand <rohitchand010904@gmail.com>
2024-08-14 13:15:13 +0530

    third commit

--- src/Components/MainBox/Mainbox.jsx
+++ src/Components/MainBox/Mainbox.jsx
@@ -5,7 +5,7 @@ const Mainbox = (props) => {
   const [value, setValue] = useState("")
   return (
     <div className='mainbox'>
-        <h1>Todo list</h1>
+        <p className='title'>TODO LIST</p>
         <div className='task'>
         <input value={value} onChange={(e) => setValue(e.target.value)} className='addtask' type='text' placeholder='Add a new task' />
         <button onClick={()=>{
@@ -13,7 +13,7 @@ const Mainbox = (props) => {
             ...prev, value
           ])
           setValue("")
-        }}>Add</button>
+        }}>Add Task</button>
         </div>      
     </div>
   )
--- src/Components/TodoTask/TodoTask.jsx
+++ src/Components/TodoTask/TodoTask.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import './TodoTask.css'
+import { MdDelete } from "react-icons/md";
 
 const TodoTask = (props) => {
   return (
-    <div className='container'>
+    props.taskList?.length > 0 && <div className='container'>
       {
         props.taskList?.map((task, idx) => {
-            return (
-                <div>
-                    {task} <button onClick={() => {
-                        props.setTaskList(prev => [
-                            ...prev.filter((_, index) => index !== idx)
-                        ])
-                    }}>delete</button>
-                </div>
-            )
+          return (
+            <div className='todo'>
+              {task} <div onClick={() => {
+                props.setTaskList(prev => [
+                  ...prev.filter((_, index) => index !== idx)
+                ])
+              }}><MdDelete /></div>
+            </div>
+          )
         })
       }
     </div>
